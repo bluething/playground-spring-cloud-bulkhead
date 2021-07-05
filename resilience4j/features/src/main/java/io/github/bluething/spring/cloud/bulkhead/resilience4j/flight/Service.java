a31fdd02814ece12c2605757c91aee0e3da0cac6
Habib Ali Machpud <habib.machpud@pegipegi.com>
2021-07-05 10:41:50 +0700

    Delayed service 1s

---
+++ resilience4j/features/src/main/java/io/github/bluething/spring/cloud/bulkhead/resilience4j/flight/Service.java
@@ -0,0 +1,32 @@
+package io.github.bluething.spring.cloud.bulkhead.resilience4j.flight;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class Service {
+    private final Random random;
+    private final DateTimeFormatter dateTimeFormatter;
+
+    public Service(Random random, DateTimeFormatter dateTimeFormatter) {
+        this.random = random;
+        this.dateTimeFormatter = dateTimeFormatter;
+    }
+
+    public List<Flight> searchFlightsTakingOneSecond(SearchRequest searchRequest) throws InterruptedException {
+        Thread.sleep(1000);
+
+        System.out.println("Searching for flights; "
+                + "current time = " + LocalDateTime.now().format(dateTimeFormatter) +
+                "; current thread = " + Thread.currentThread().getName());
+
+        List<Flight> flights = Arrays.asList(new Flight("XY 765", searchRequest.getFlightDate(), searchRequest.getFrom(), searchRequest.getTo()),
+                new Flight("XY 746", searchRequest.getFlightDate(), searchRequest.getFrom(), searchRequest.getTo()));
+
+        System.out.println("Flight search successful at " + LocalDateTime.now().format(dateTimeFormatter));
+
+        return flights;
+    }
+}
