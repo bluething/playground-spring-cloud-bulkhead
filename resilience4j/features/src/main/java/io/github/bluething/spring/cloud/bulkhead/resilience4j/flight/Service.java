db1274aea8bbb61513dadc19c81f8da5ce4a38bf
Habib Ali Machpud <habib.machpud@pegipegi.com>
2021-07-05 11:03:35 +0700

    Search flight with random delay

--- resilience4j/features/src/main/java/io/github/bluething/spring/cloud/bulkhead/resilience4j/flight/Service.java
+++ resilience4j/features/src/main/java/io/github/bluething/spring/cloud/bulkhead/resilience4j/flight/Service.java
@@ -1,5 +1,6 @@
 package io.github.bluething.spring.cloud.bulkhead.resilience4j.flight;
 
+import javax.swing.plaf.TableHeaderUI;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
@@ -17,6 +18,21 @@ public class Service {
 
     public List<Flight> searchFlightsTakingOneSecond(SearchRequest searchRequest) throws InterruptedException {
         Thread.sleep(1000);
+
+        System.out.println("Searching for flights; "
+                + "current time = " + LocalDateTime.now().format(dateTimeFormatter) +
+                "; current thread = " + Thread.currentThread().getName());
+
+        List<Flight> flights = Arrays.asList(new Flight("XY 765", searchRequest.getFlightDate(), searchRequest.getFrom(), searchRequest.getTo()),
+                new Flight("XY 746", searchRequest.getFlightDate(), searchRequest.getFrom(), searchRequest.getTo()));
+
+        System.out.println("Flight search successful at " + LocalDateTime.now().format(dateTimeFormatter));
+
+        return flights;
+    }
+
+    public List<Flight> searchFlightsTakingRandomTime(SearchRequest searchRequest) throws InterruptedException {
+        Thread.sleep(random.nextInt(3000));
 
         System.out.println("Searching for flights; "
                 + "current time = " + LocalDateTime.now().format(dateTimeFormatter) +
