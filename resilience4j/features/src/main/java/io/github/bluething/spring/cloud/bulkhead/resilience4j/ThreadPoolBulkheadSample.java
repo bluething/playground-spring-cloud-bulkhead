3bb9c8ba73cbc0a8de04d5f71d1dfc0e14b4d0ba
Habib Ali Machpud <habib.gunadarma@gmail.com>
2021-07-06 15:00:28 +0700

    Simulate BulkFullException

--- resilience4j/features/src/main/java/io/github/bluething/spring/cloud/bulkhead/resilience4j/ThreadPoolBulkheadSample.java
+++ resilience4j/features/src/main/java/io/github/bluething/spring/cloud/bulkhead/resilience4j/ThreadPoolBulkheadSample.java
@@ -3,7 +3,6 @@ package io.github.bluething.spring.cloud.bulkhead.resilience4j;
 import io.github.bluething.spring.cloud.bulkhead.resilience4j.flight.Flight;
 import io.github.bluething.spring.cloud.bulkhead.resilience4j.flight.SearchRequest;
 import io.github.bluething.spring.cloud.bulkhead.resilience4j.flight.Service;
-import io.github.resilience4j.bulkhead.Bulkhead;
 import io.github.resilience4j.bulkhead.ThreadPoolBulkhead;
 import io.github.resilience4j.bulkhead.ThreadPoolBulkheadConfig;
 import io.github.resilience4j.bulkhead.ThreadPoolBulkheadRegistry;
@@ -63,6 +62,45 @@ public class ThreadPoolBulkheadSample {
                     });
         }
     }
+    void threadPoolWithException() {
+        ThreadPoolBulkheadConfig threadPoolBulkheadConfig = ThreadPoolBulkheadConfig.custom()
+                .maxThreadPoolSize(2)
+                .coreThreadPoolSize(1)
+                .queueCapacity(1)
+                .build();
+        ThreadPoolBulkheadRegistry threadPoolBulkheadRegistry = ThreadPoolBulkheadRegistry.of(threadPoolBulkheadConfig);
+        ThreadPoolBulkhead bulkhead = threadPoolBulkheadRegistry.bulkhead("flightSearchService");
+
+        Random random = new Random();
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss SSS");
+        Service flightSearchService = new Service(random, dateTimeFormatter);
+
+        SearchRequest searchRequest = new SearchRequest("NYC", "LAX", "07/06/2021");
+
+        Supplier<List<Flight>> flightSupplier = () -> {
+            List<Flight> flights = new ArrayList<>();
+            try {
+                flights = flightSearchService.searchFlightsTakingOneSecond(searchRequest);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return flights;
+        };
+        Supplier<CompletionStage<List<Flight>>> decoratedFlightSupplier = ThreadPoolBulkhead.decorateSupplier(bulkhead, flightSupplier);
+
+        for (int i = 0; i < 4; i++) {
+            decoratedFlightSupplier
+                    .get()
+                    .whenComplete((r, t) -> {
+                        if (r != null) {
+                            System.out.println("Received results");
+                        }
+                        if (t != null) {
+                            t.printStackTrace();
+                        }
+                    });
+        }
+    }
 
     public static void main(String[] args) throws InterruptedException {
         ThreadPoolBulkheadSample threadPoolBulkhead = new ThreadPoolBulkheadSample();
@@ -70,9 +108,17 @@ public class ThreadPoolBulkheadSample {
         System.out.println(" ====== ");
         threadPoolBulkhead.threadPoolBasicUsage();
         delay(5);
+        System.out.println(" ====== ");
+        threadPoolBulkhead.threadPoolWithException();
+        delay(10);
+        System.out.println(" ====== ");
     }
 
-    static void delay(int second) throws InterruptedException {
-        Thread.sleep(second * 1000);
+    static void delay(int second) {
+        try {
+            Thread.sleep(second * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
